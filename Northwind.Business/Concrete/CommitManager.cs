2a558c7e1838fc7bc314c70914814037d05180bc
serifahmetharas <serifahmetharas@gmail.com>
2022-01-19 16:28:07 +0300

    git desktop commit denemesidir.
    commit denemesi

---
+++ Northwind.Business/Concrete/CommitManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    class CommitManager
+    {
+        int a;
+
+        // Tamamiyle GithubDesktop uygulaması denemek icin olusturulmus bir class.
+    }
+}
