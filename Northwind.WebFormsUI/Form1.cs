9dac56cae8f2807cdc9b62edb96e279d39514374
serifahmetharas <91914710+serifahmetharas@users.noreply.github.com>
2022-01-19 15:52:10 +0300

    Add files via upload

--- Northwind.WebFormsUI/Form1.cs
+++ Northwind.WebFormsUI/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Northwind.Business.Abstract;
 using Northwind.Business.Concrete;
+using Northwind.DataAccess.Concrete.Entity_Framework;
 using Northwind.DataAccess.Concrete.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -19,18 +20,69 @@ namespace Northwind.WebFormsUI
         {
             InitializeComponent();
             _productService = new ProductManager(new EfProductDal());
+            _categoryService = new CategoryManager(new EfCategoryDal());
         }
-         
+
         private void dgwProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
 
         }
 
-        private IProductService _productService;  
+
         private void Form1_Load(object sender, EventArgs e)
-        {  
+        {
+            LoadProducts(); // Ürünlerin gelmesi için kendimizin oluşturduğu bir method.
+            LoadCategories();
+        }
+        private IProductService _productService;
+        private ICategoryService _categoryService;
+        private void LoadCategories()
+        {
+            cbxCategory.DataSource = _categoryService.GetAll();
+            cbxCategory.DisplayMember = "CategoryName";
+            cbxCategory.ValueMember = "CategoryId";
+        }
+
+        private void LoadProducts()
+        {
             dgwProduct.DataSource = _productService.GetAll();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbxProductName.Text))
+            {
+                dgwProduct.DataSource = _productService.GetProductsByName(tbxProductName.Text);
+            }
+            else
+            {
+                LoadProducts();
+            }
+            
+        }
+
+        private void lblCategory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dgwProduct.DataSource = _productService.GetProductsByCategory(Convert.ToInt32(cbxCategory.SelectedValue));
+            }
+            catch
+            {
+                
+            }
+            
         }
     }
 }
